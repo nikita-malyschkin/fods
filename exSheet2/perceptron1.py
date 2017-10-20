686f48e5a6a0f8addffed6805241cb6c40c925de
Valentin Engelke <nevaltin@github.com>
2017-10-20 21:46:09 +0200

    Exercise 1,beginning of 2

---
+++ exSheet2/perceptron1.py
@@ -0,0 +1,20 @@
+import numpy as np
+w=np.array([0,0,0])
+x=np.array([[4,2,1],[-1,2,-4],[8,-2,-3],[1,-1,1],[2,-2,5],[-6,2,7]])
+y=np.array([-1,1,1,-1,-1,-1])
+finished=False
+
+while (finished==False):
+    finished=True
+    steps=0
+    for t in range(0,6):    
+        if (np.sign(np.dot(w,x[t]))!=y[t]):
+            w=w+y[t]*x[t]
+            steps+=1
+            print(w)    
+            finished=False
+print("Finished in "+str(steps)+" steps")
+
+    
+            
+
