069f5900cdc3bfdabd699f6a09c719f917911643
Valentin Engelke <valentin.engelke@gmail.com>
2017-10-24 17:15:02 +0200

    cluster update

--- exSheet2/cluster.py
+++ exSheet2/cluster.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 def main():
 	S=np.array([[2,12],[3,11],[3,8],[5,4],[7,5],[7,3],[10,8],[13,8]])
 	z=np.array([[2,12],[3,11],[3,8]])
@@ -19,7 +20,37 @@ def main():
 			changed=False
 		z=newCenter(S,clus)
 		print(z)
-	
+
+
+	x=[]
+	y=[]
+	xz=[]
+	yz=[]
+	for a in range(len(S)):
+		x.append(S[a][0])
+		y.append(S[a][1])
+	for a in range(len(z)):
+			xz.append(z[a][0])
+			yz.append(z[a][1])
+	plt.scatter(x,y)
+	plt.scatter(xz,yz)
+	t1=np.arange(-5.0,15.0,0.1)
+	plt.plot(t1,f12(t1))
+	plt.plot(t1,f13(t1))
+	plt.plot(t1,f23(t1))
+	axes = plt.gca()
+	axes.set_xlim([0,15])
+	axes.set_ylim([0,15])
+	n=['A','B','C,P2','D','E','F','G','H']
+	for i, txt in enumerate(n):
+
+   		axes.annotate(txt, (x[i],y[i]))
+   	n2=['P1','','P3']
+   	for i, txt in enumerate(n2):
+
+   		axes.annotate(txt, (xz[i],yz[i]))
+
+	plt.show()
 
 
 def newCenter(S,clus):
@@ -30,6 +61,13 @@ def newCenter(S,clus):
 	for x in range(0,3):
 		sum[x]= sum[x]/np.count_nonzero(clus==x)
 	return sum
+
+def f12(x):
+	return 1.0/32.0 * (4*x+309)
+def f13(x):
+	return (1180.0*x+4833)/1280
+def f23(x):
+	return 1.0/40.0*(90*x-241)
 
 
 if __name__ == '__main__':
