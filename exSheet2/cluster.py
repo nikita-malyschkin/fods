a6f393f50fe75afa04b19476a9e871dcd1697221
Valentin Engelke <valentin.engelke@gmail.com>
2017-10-24 10:11:54 +0200

    cluster.py working?

--- exSheet2/cluster.py
+++ exSheet2/cluster.py
@@ -1,32 +1,36 @@
 import numpy as np
-import matplotlib.pyplot as plt
-class Point:
-	def __init__(self,a,b):
-		self.x = a
-		self.y = b
+def main():
+	S=np.array([[2,12],[3,11],[3,8],[5,4],[7,5],[7,3],[10,8],[13,8]])
+	z=np.array([[2,12],[3,11],[3,8]])
+	clus=np.zeros(8)
 
-a=Point(2,12)
-b=Point(3,11)
-c=Point(3,8)
-d=Point(5,4)
-e=Point(7,5)
-f=Point(7,3)
-g=Point(10,8)
-h=Point(13,8)
+	changed=True
+	while(changed==True):
+		lclus=clus	
+		clus=np.zeros(8)
+		for x in range(0,8):
+			dist=[0,0,0]
+			for t in range(0,3):
+				dist[t]=np.linalg.norm(S[x]-z[t])
+			clus[x]=np.argmin(dist)
+		print(clus)
+		print(lclus)
+		if(all(clus==lclus)):
+			changed=False
+		z=newCenter(S,clus)
+		print(z)
+	
 
-l=[a,b,c,d,e,f,g,h]
-z=[a,b,c]
-z[0].x+=1
-print(z[0].x)
-print(a[0].x)
-change=True
-while change :
-	change=False
 
-#x=[]
-#y=[]
-#for p in l:
-#	x.append(p.x)
-#	y.append(p.y)
-#plt.scatter(x,y)
-#plt.show()
+def newCenter(S,clus):
+	sum= np.zeros((3,2))
+	for x in range(len(clus)):
+		sum[int(clus[x])]+=S[x]
+	
+	for x in range(0,3):
+		sum[x]= sum[x]/np.count_nonzero(clus==x)
+	return sum
+
+
+if __name__ == '__main__':
+    main()
