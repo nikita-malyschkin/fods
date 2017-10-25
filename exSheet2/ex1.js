92ae850fa749154ca7287c99020d44303380dbc4
Nikita Malyschkin <nikita.malyschkin@gmail.com>
2017-10-25 13:59:31 +0200

    sheet2ex1: added homogeneous

--- exSheet2/ex1.js
+++ exSheet2/ex1.js
@@ -8,15 +8,26 @@ const vectorScale = (v, a) => v.map(val => val * a);
 
 const euclideanNorm = v => Math.sqrt(v.reduce((prev, curr) => prev + curr * curr, 0));
 
+const homogenizeSequence = S => S.map(entry => [[...entry[0], 1], entry[1]]);
+
 const maxNormforSequence = S => S.reduce((prev, curr) => Math.max(prev, euclideanNorm(curr[0])), 1);
 
 const normalizeSequence = S => S.map(item => [item[0].map(val => val / maxNormforSequence(S)), item[1]]);
 
+const shuffle = a => {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+};
+
 let done = false;
 let steps = 0;
-let w = [0, 0, 0];
+let w = [0, 0, 0, 0];
 const S = [[[4, 2, 1], -1], [[-1, 2, -4], 1], [[8, -2, -3], 1], [[1, -1, 1], -1], [[2, -2, 5], -1], [[-6, 2, 7], -1]];
-const normalizedS = normalizeSequence(S);
+// shuffle(S);
+const homogeneousS = homogenizeSequence(S);
+const normalizedS = normalizeSequence(homogeneousS);
 
 while (!done) {
     done = true;
@@ -30,3 +41,4 @@ while (!done) {
 }
 
 console.log(w.join("\t"));
+console.log("in", steps, "steps");
